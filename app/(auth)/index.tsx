45fc277e6a45d781883511b9c6f0f00aa4ab0fc3
MhayreeJhaine <mhayreejhaine@gmail.com>
2026-03-08 10:30:55 +0100

    feat: extended props for button flexibility

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,2 +1,35 @@
-const Index = () => {};
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Button from "../../components/ui/button";
+
+const Index = () => {
+  return (
+    <SafeAreaView className="p-4 flex-1">
+      <View className="flex-1 justify-between ">
+        <View>
+          <Text className="text-4xl font-PoppinsBold">Welcome</Text>
+          <Text className="text-lg font-Poppins text-gray-500 mt-2">
+            Sign up or Log in to continue
+          </Text>
+        </View>
+        <View>
+          <Image source={require("../../assets/images/authIndex.png")} />
+        </View>
+
+        {/* btns */}
+        <View className="mb-4">
+          <Button title="Sign up" route="/(auth)/signup" />
+          <Button
+            title="Log in"
+            onPress={() => console.log("login clicked")}
+            backgroundColor="transparent"
+            borderColor="#CF08E9"
+            border="1"
+            color="#CF08E9"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 export default Index;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,18 +1,39 @@
 import { Href, useRouter } from "expo-router";
-import { DimensionValue, Text, TouchableOpacity } from "react-native";
+import {
+  ActivityIndicator,
+  DimensionValue,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 
 type ButtonProps = {
   title?: string;
   onPress?: () => void;
   route?: Href;
   width?: DimensionValue;
+  backgroundColor?: string;
+  borderColor?: string;
+  border?: string;
+  color?: string;
+  className?: string;
+  disabled?: boolean;
+  opacity?: number;
+  loading?: boolean;
 };
 
 const Button = ({
-  title,
+  title = "Continue",
   onPress,
   route,
   width = "100%" as DimensionValue,
+  backgroundColor = "#CF08E9",
+  borderColor = "#CF08E9",
+  border = "none",
+  color = "#fff",
+  className,
+  disabled = false,
+  opacity = disabled ? 0.5 : 1,
+  loading = false,
 }: ButtonProps) => {
   const router = useRouter();
 
@@ -27,11 +48,29 @@ const Button = ({
 
   return (
     <TouchableOpacity
-      className="items-center justify-center bg-primary py-4 rounded-[35px]"
+      className={`items-center justify-center py-4 rounded-[35px] mb-4 ${className}`}
       onPress={handlePress}
-      style={{ width }}
+      style={{
+        width,
+        backgroundColor,
+        borderColor,
+        borderWidth: border === "none" ? 0 : 1,
+        opacity,
+      }}
+      disabled={disabled}
     >
-      <Text className="text-white font-Poppins text-lg">{title}</Text>
+      {loading ? (
+        <ActivityIndicator color={"#fff"} size={30} />
+      ) : (
+        <Text
+          className={`font-Poppins text-xl ${className}`}
+          style={{
+            color,
+          }}
+        >
+          {title}
+        </Text>
+      )}
     </TouchableOpacity>
   );
 };
